596334796e43b56e3423a3a8c80ec0a895d1a0d8
paisolitario <tatawmarques060606@gmail.com>
2023-11-23 10:17:18 -0400

    PROVA FINAL

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
-import express from 'express'
-const app = express()
-app.use(express.json())
 import ListaFilme from './aplicacao/lista-filme.use-case'
 import BancoMongoDB from './infra/banco/banco-mongodb'
+import SalvaFilme from './aplicacao/salva-filme.use-case'
+import express from 'express'
+import cors from 'cors'
+//npm install cors --save
+const app = express()
+app.use(express.json())
+app.use(cors())
+
 const bancoMongoDB = new BancoMongoDB()
 app.get('/filmes',async (req,res)=>{
     //usem o listarFilme Usecase para listar os filmes
@@ -10,7 +15,7 @@ app.get('/filmes',async (req,res)=>{
     const filmes = await listaFilme.executar()
     res.send(filmes)
 })
-app.post('/filmes',(req,res)=>{
+app.post('/filmes',async (req,res)=>{
     const {id,titulo,descricao,imagem} = req.body
     const filme = {
         id,
@@ -19,8 +24,13 @@ app.post('/filmes',(req,res)=>{
         imagem
     }
     //Como eu salvo o filme que foi cadastrado no meu vetor de filmes (Banco de dados)
-    filmesCadastros.push(filme)
+    const salvaFilme = new SalvaFilme(bancoMongoDB)
+    const resposta = await salvaFilme.execute(filme)
     res.status(201).send(filme)
+    // try{
+    // }catch(error){
+    //     res.status(400).send({"mensagem":"Erro ao cadastrar o filme"})
+    // }
 })
 
 app.get('/filmes/:id',(req,res)=>{
